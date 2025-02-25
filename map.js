38a8c7ee3ce907316d32ee0507ef6fc388263d47
Eric Cheng <chengeric155@gmail.com>
2025-02-24 23:42:41 -0800

    step 4

--- map.js
+++ map.js
@@ -60,6 +60,29 @@ map.on('load', async () => {
 
     let stations = jsonData.data.stations;
     console.log('Stations Array:', stations);
+    const trips = await d3.csv('https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv');
+    const departures = d3.rollup(
+        trips,
+        (v) => v.length,
+        (d) => d.start_station_id,
+    );
+    const arrivals = d3.rollup(
+        trips,
+        (v) => v.length,
+        (d) => d.end_station_id,
+    );
+    stations = stations.map((station) => {
+        let id = station.short_name;
+        station.arrivals = arrivals.get(id) ?? 0;
+        station.departures = departures.get(id) ?? 0;
+        station.totalTraffic = station.arrivals + station.departures;
+        return station;
+    });
+
+    const radiusScale = d3
+        .scaleSqrt()
+        .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+        .range([0, 25]);
 
     const svg = d3.select('#map').select('svg');
 
@@ -68,11 +91,17 @@ map.on('load', async () => {
         .data(stations)
         .enter()
         .append('circle')
-        .attr('r', 5)               // Radius of the circle
+        .attr('r', d => radiusScale(d.totalTraffic)) // Radius of the circle
         .attr('fill', 'steelblue')  // Circle fill color
         .attr('stroke', 'white')    // Circle border color
         .attr('stroke-width', 1)    // Circle border thickness
-        .attr('opacity', 0.8);      // Circle opacity
+        .attr('opacity', 0.8)      // Circle opacity
+        .each(function(d) {
+            // Add <title> for browser tooltips
+            d3.select(this)
+              .append('title')
+              .text(`${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`);
+        });
     // Function to update circle positions when the map moves/zooms
 
     function updatePositions() {
@@ -81,14 +110,16 @@ map.on('load', async () => {
           .attr('cy', d => getCoords(d).cy); // Set the y-position using projected coordinates
       }
   
-      // Initial position update when map loads
-    //updatePositions(); <- previous code
+    // Initial position update when map loads
+    updatePositions();
 
     // Reposition markers on map interactions
     map.on('move', updatePositions);     // Update during map movement
     map.on('zoom', updatePositions);     // Update during zooming
     map.on('resize', updatePositions);   // Update on window resize
     map.on('moveend', updatePositions);  // Final adjustment after movement ends
+
+
 });
 
 
@@ -96,4 +127,5 @@ function getCoords(station) {
     const point = new mapboxgl.LngLat(+station.lon, +station.lat);  // Convert lon/lat to Mapbox LngLat
     const { x, y } = map.project(point);  // Project to pixel coordinates
     return { cx: x, cy: y };  // Return as object for use in SVG attributes
-  }
+}
+
